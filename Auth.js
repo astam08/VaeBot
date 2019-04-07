c8ab77ddc28fe24434a476754ae516920eb388fa
astam08 <49343371+astam08@users.noreply.github.com>
2019-04-08 01:49:06 +0700

    Create Auth.js

---
+++ Auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+discordToken: 'NTYyNTI5MzkzNjAwMTAyNDAx.XKo4ug.S5Jn6ZeBdA1Z74JagrvKHzok_i0',
+youtube: 'AIzaSyDtnhiJiY9vZk-5rGwSDnGY8LyiaNfQRWs',
+trelloKey: 'Trello API key',
+trelloToken: 'Trello API token',
+dbPass: 'Database password',
+};
